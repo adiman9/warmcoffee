50fbc120f354eab2d07a1f4e8c3db56e5cc53b14
Adrian Henry <adrian@hungryturtlecode.com>
2018-04-26 19:09:11 +0100

    add ability to follow/unfollow and display posts from users you followed in a paginated fashion

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['adrian@hungryturtlecode.com']
+    POSTS_PER_PAGE = 25
